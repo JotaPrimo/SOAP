f1a1ab59589433c3822c9e71ebe3e233e8b8cc72
JotaPrimo <gestald118@gmail.com>
2023-06-26 19:39:25 -0400

    Consumindo um serviço de terceiro baseado em SOAP

---
+++ correios/src/main/java/com/jotasantos/correios/App.java
@@ -0,0 +1,33 @@
+package com.jotasantos.correios;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import javax.xml.namespace.QName;
+
+import com.fourcatsdev.correios.servico.AtendeCliente;
+import com.fourcatsdev.correios.servico.EnderecoERP;
+
+import jakarta.xml.ws.Service;
+
+
+
+public class App 
+{
+    public static void main( String[] args ) throws Exception  
+    {
+	URL urlCooreios = new URL("https://apps.correios.com.br/SigepMasterJPA/AtendeClienteService/AtendeCliente?wsdl");
+	        
+	        QName qnameCorreios = new QName("http://cliente.bean.master.sigep.bsb.correios.com.br/", "AtendeClienteService");
+	  
+	        Service serviceCorreios = Service.create(urlCooreios, qnameCorreios);
+	        
+	        AtendeCliente correios = serviceCorreios.getPort(AtendeCliente.class);
+	        
+	        EnderecoERP endereco = correios.consultaCEP("79103400");
+	        System.out.println(endereco.getCidade());
+	        System.out.println(endereco.getBairro());
+
+    }
+    
+}
